493b6825304cdf47e7239d92a0b41a10f17a7bd2
Minh Anh Nguyen <minhanhn@usf.edu>
2025-09-30 17:09:32 -0400

    created class, added constructor() for parameters and describe() as methods

---
+++ cc_6.js
@@ -0,0 +1,10 @@
+// Step 2
+class Employee {
+    constructor(name, department) {
+        this.name = name;
+        this.department = department;
+    }
+    describe(){
+        return `${this.name} works in ${this.department}`
+    }
+}
